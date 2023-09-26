2bc9c31b91bd589c9f4e8b499b186f0570b9660b
Jxshvfx <spiteyhd@gmail.com>
2023-09-26 15:49:58 -0400

    creation objets (debut)

---
+++ script.js
@@ -0,0 +1,18 @@
+let debut = {
+    titre: "activation",
+    description: "Vous êtes un robot destinée à sauvé l'humanité d'une invasion d'inteligence articficielle qui se passera dans 100 ans. Vous devrez altérer des moments importants de l'histoire pour empêcher cela d'arriver.",
+    image: "./assets/mise-en-forme.jpg",
+    boutons: []
+};
+
+let infiltraton = {
+    titre: "infiltration",
+    description: "Lors d'une infiltration d'une base qui était grandement impliqué à la fabrication des robots. Vous devez "
+}
+
+let chapters = {
+    debut: debut,
+    infiltraton: infiltraton,
+};
+
+console.log(chapters);
