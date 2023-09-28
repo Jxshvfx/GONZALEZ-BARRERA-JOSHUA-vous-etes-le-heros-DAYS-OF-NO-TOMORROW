432e35dd6cd9c1b84e12211b9e9fd8e697e6e8bb
Jxshvfx <spiteyhd@gmail.com>
2023-09-28 11:10:20 -0400

    code Chapters et image

--- script.js
+++ script.js
@@ -1,18 +1,85 @@
-let debut = {
-    titre: "activation",
-    description: "Vous êtes un robot destinée à sauvé l'humanité d'une invasion d'inteligence articficielle qui se passera dans 100 ans. Vous devrez altérer des moments importants de l'histoire pour empêcher cela d'arriver.",
-    image: "./assets/mise-en-forme.jpg",
-    boutons: []
-};
-
-let infiltraton = {
-    titre: "infiltration",
-    description: "Lors d'une infiltration d'une base qui était grandement impliqué à la fabrication des robots. Vous devez "
-}
-
 let chapters = {
-    debut: debut,
-    infiltraton: infiltraton,
-};
+    debut: {
+        titre: "activation",
+        description: "Vous êtes un robot destinée à sauvé l'humanité d'une invasion d'inteligence articficielle (autres robots) qui se passera dans 100 ans. Vous devrez altérer des moments importants de l'histoire pour empêcher cela d'arriver.",
+        image: "./assets/mise-en-forme.jpg",
+        boutons: [{
+            titre: "Activez.",
+            destination: "infiltration"
+        }]
+    },
+    infiltraton: {
+        titre: "infiltration",
+        description: "Lors d'une infiltration d'une base qui était grandement impliqué à la fabrication des robots vous vous demandez comment vous voulez l'infiltrez. Discrètement ou d'une manière explosive?",
+        image: "./assets/img_base.png",
+        boutons: [{}]
+    },
 
-console.log(chapters);
+    explosion: {
+        titre: "explosion",
+        description: "Vous décidez d'infiltrez d'une façon totalement explosive et vous réussisez à détruire la base au complète. Un pas vers l'avant pour votre but.",
+        image: "",
+        boutons: [{}]
+    },
+
+    discretion: {
+        titre: "discretion",
+        description: "Vous décidez d'infiltrez discrètement mais vous n'avez aucune capacité à restez discret et vous vous faites trouver et vous êtes détruit.",
+        image: "",
+        boutons: [{}]
+    },
+
+    choixDifficile: {
+        titre: "choixDifficile",
+        description: "En continuant votre mission, vous voyez qu'un avion explosera lors de son départ et tuera tous les passagers qui inclue une petite fille que vous avez aidé et qui vous supporte. Décidez-vous d'altérez l'histoire et la sauvez ou ne rien faire et continuer.",
+        image: "",
+        boutons: [{}]
+    },
+
+    alterer: {
+        titre: "alteration",
+        description: "Vous décidez d'altérez l'histoire, mais il semble être le mauvais choix car vous sauvez aussi un supporteur pour les robots qui accélère l'éventuelle invasion de robots et l'humanité meurt.",
+        image: "./assets/img_aviondetruit.jpg",
+        boutons: [{}]
+    },
+
+    rienFaire: {
+        titre: "rienFaire",
+        description: "Vous décidez de ne rien faire et laissez la fille mourir. Un sacrifice inévitable.",
+        image: "",
+        boutons: [{}]
+    },
+
+    escapade: {
+        titre: "escapade",
+        description: "Lors d'une escapade, vous voyez un docteur pris au piège par des robots, décidez-vous de le sauvez avec un pistolet, décidez-vous de le sauvez avec vos arts martiaus ou décidez vous de partir?",
+        image: "",
+        boutons: [{}]
+    },
+
+    pistolet: {
+        titre: "pistolet",
+        description: "Vous décidez de le sauvez avec un pistolet d'un garde. Cependant, vous n'avez pas de capacité à utiliser des pistolets et les robots vous tuent.",
+        image: "",
+        boutons: [{}]
+    },
+
+    artMartiaux: {
+        titre: "artMartiaux",
+        description: "Vous décidez de le sauvez avec vos arts martiaux. Vous désarmez tout les robots et les détruiser. Le docteur vous remercie et vous échappez ensemble.",
+        image: "",
+        boutons: [{}]
+    },
+
+    echappez: {
+        titre: "echappez",
+        description: "Vous décidez de vous en allez et de laisser le docteur de survivre par lui même.",
+        image: "",
+        boutons: [{}]
+    },
+
+    endgame: {
+        titre: "endgame",
+        description: ""
+    }
+};
